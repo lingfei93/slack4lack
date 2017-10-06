b26988aca8111066f6bde3e4133c787dae86d131
khorleyi <khorleyi@berkeley.edu>
2017-10-06 18:44:31 +0800

    added

---
+++ slack4lack/test.swift
@@ -0,0 +1,13 @@
+//
+//  test.swift
+//  slack4lack
+//
+//  Created by Khor Le Yi on 6/10/17.
+//  Copyright © 2017 Khor Le Yi. All rights reserved.
+//
+
+import Cocoa
+
+class test: NSObject {
+
+}
