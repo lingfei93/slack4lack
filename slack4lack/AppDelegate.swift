41c50aa34f4e488e1c0a232814a573d737e83ef2
khorleyi <khorleyi@berkeley.edu>
2017-10-06 18:25:25 +0800

    Initial Commit

---
+++ slack4lack/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  slack4lack
+//
+//  Created by Khor Le Yi on 6/10/17.
+//  Copyright © 2017 Khor Le Yi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ slack4lack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  slack4lack
+//
+//  Created by Khor Le Yi on 6/10/17.
+//  Copyright © 2017 Khor Le Yi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
