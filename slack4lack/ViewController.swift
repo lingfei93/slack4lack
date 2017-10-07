73675abe24873e8794d84f216c93fceef281cdbc
khorleyi <khorleyi@berkeley.edu>
2017-10-07 13:07:05 +0800

    test

--- slack4lack/ViewController.swift
+++ slack4lack/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("hi hi")
+        print ("hi hi")
         // Do any additional setup after loading the view.
     }
 
